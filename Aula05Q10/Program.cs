c328e9eca834f8b1f2f38fc30bc06e9ba62cb526
AleOi2 <alexandre.jtsubaki@gmail.com>
2022-08-17 18:55:24 -0300

    Enviando..

---
+++ Aula05Q10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula05Q10
+{
+
+    internal class Program
+    {
+        delegate string StringOperacao(string s);
+        static void Main(string[] args)
+        {
+            StringOperacao inverter = delegate (string s)
+            {
+                char[] charArray = s.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+                //return "Verificando";
+            };
+            Console.WriteLine(inverter("C# é diferente"));
+        }
+    }
+}
