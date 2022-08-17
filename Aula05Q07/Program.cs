cd9135b96885453e864a7b0ee0350e69ee022254
AleOi2 <alexandre.jtsubaki@gmail.com>
2022-08-17 18:27:39 -0300

    Enviando..

---
+++ Aula05Q07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula05Q07
+{
+    delegate double Operacao(double x, double y);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Operacao soma = (x, y) => x + y;
+            Operacao sub = (x, y) => x - y;
+            Operacao mult = (x, y) => x * y;
+            Console.WriteLine(soma(2,5));
+            Console.WriteLine(sub(2, 5));
+            Console.WriteLine(mult(2, 5));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula05Q08/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula05Q08
+{
+    class UsandoDelegate
+    {
+        delegate double Soma(double a, double b);
+        delegate void imprimirSoma(double a, double b);
+
+        static double MinhaSoma(double x, double y)
+        {
+            return x + y;
+        }
+
+        static void MeuImprimirSoma(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static void Main(string[] args)
+        {
+            Soma op1 = MinhaSoma;
+            Console.WriteLine(op1(2, 3.9));
+            imprimirSoma op2 = MeuImprimirSoma;
+            op2(5.4, 8);
+            Func<double, double, double> op3 = MinhaSoma;
+            Console.WriteLine(op3(2.5,3));
+            Action<double, double> op4 = MeuImprimirSoma;
+            op4(7.7, 23.4);
+            
+        }
+
+    }
+}
